d530a3a29d098e5699649ddc99e3858bd8abf81f
tBrause <brause.torsten@gmail.com>
2022-06-09 21:42:39 +0200

    new getter

--- get-mqtt.js
+++ get-mqtt.js
@@ -18,33 +18,19 @@ const client = mqtt.connect(connectUrl, {
 });
 
 // TOPIC
-const topicDir = "home/temperatur";
+const topic = "home/temperatur";
 
 // CONNECT TOPIC
 client.on("connect", () => {
-  console.log("SET : CONNECT : " + connectUrl);
-
-  // VALUE
-  let value = "2";
-
+  console.log("GET : CONNECT : " + connectUrl);
   // SUBSCRIBE
-  client.subscribe([topicDir], () => {
-    console.log(`SUBSCRIBE : topic : ${topicDir}`);
-  });
-
-  // PUBLISH
-  client.publish(topicDir, value, { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(`PUBLISH : Topic : ${topicDir} : ${value}`);
-      //console.log(`Value : ${value}`);
-    }
+  client.subscribe([topic], () => {
+    console.log(`SUBSCRIBE : topic : ${topic}`);
+    console.log("");
   });
 });
 
 // MESSAGE
 client.on("message", (topic, payload) => {
   console.log("MESSAGE : ", topic, payload.toString());
-  console.log("");
 });
